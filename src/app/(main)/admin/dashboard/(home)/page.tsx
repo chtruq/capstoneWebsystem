a589c196f46edd3f757b2bf94703dc0006a822eb
chtruq <trunghcw@gmail.com>
2024-12-30 09:20:52 +0700

    deploy

--- src/app/(main)/admin/dashboard/(home)/page.tsx
+++ src/app/(main)/admin/dashboard/(home)/page.tsx
@@ -6,7 +6,7 @@ function HomePage() {
   const breadcrumbItems = [
     { label: "Home", href: "/" },
     { label: "More", dropdownItems: ["Documentation", "Themes", "GitHub"] },
-    { label: "Components", href: "/docs/components" },
+    // { label: "Components", href: "/docs/components" },
     // { label: "Breadcrumb" }, // Current page
   ];
   return (
